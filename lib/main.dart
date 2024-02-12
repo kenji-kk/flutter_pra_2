aabb451cb3bbd1bc87ed0092b391b7fbda0afbe3
Kakehi Keigo <kenjix12x22@gmail.com>
2024-02-13 03:44:39 +0900

    class切り出し

--- lib/main.dart
+++ lib/main.dart
@@ -12,28 +12,41 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("my birthday"),
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: '名前',
-                    hintText: '名前',
-                    icon: Icon(Icons.account_circle)),
-              ),
+        body: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: '名前',
+                  hintText: '名前',
+                  icon: Icon(Icons.account_circle)),
             ),
-            DatePickerDialog(
-                initialDate: DateTime(DateTime.now().year - 10),
-                firstDate: DateTime(DateTime.now().year - 100),
-                lastDate: DateTime(DateTime.now().year)),
-            ElevatedButton(
-                onPressed: () {
-                  print("ボタンを押しました");
-                },
-                child: Text("生まれてから何日目？"))
-          ],
-        ),
+          ),
+          DatePickerDialog(
+              initialDate: DateTime(DateTime.now().year - 10),
+              firstDate: DateTime(DateTime.now().year - 100),
+              lastDate: DateTime(DateTime.now().year)),
+          ElevatedButton(
+              onPressed: () {
+                print("ボタンを押しました");
+              },
+              child: Text("生まれてから何日目？"))
+        ],
       ),
     );
   }
